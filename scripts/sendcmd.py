f7d6763fbbec1a482f569fadaad133554a2cba4e
buddyjojo <buddyjojo06@outlook.com>
2026-04-12 09:23:52 -0400

    Add argv to sendcmd
    Yes I know it's python, I'm still gonna call it argv anyways

--- scripts/sendcmd.py
+++ scripts/sendcmd.py
@@ -1,21 +1,25 @@
+#!/usr/bin/env python3
+import argparse
 import socket
 import struct
 
-TCP_IP = '10.42.0.140'
-TCP_PORT = 902
+# TCP_IP = '10.42.0.140'
+# TCP_PORT = 902
 
 BUFFER_SIZE = 1
 
-ISD1200_INIT = 0xA0
-ISD1200_DEINIT = 0xA1
-ISD1200_READ_ID = 0xA2
-ISD1200_READ_FLASH = 0xA3
-ISD1200_ERASE_FLASH = 0xA4
-ISD1200_WRITE_FLASH = 0xA5
-ISD1200_PLAY_VOICE = 0xA6
-ISD1200_EXEC_MACRO = 0xA7
-ISD1200_RESET = 0xA8
-ISD1200_PLAYSPI = 0xA9
+COMMANDS = {
+    "INIT": 0xA0,
+    "DEINIT": 0xA1,
+    "READ_ID": 0xA2,
+    "READ_FLASH": 0xA3,
+    "ERASE_FLASH": 0xA4,
+    "WRITE_FLASH": 0xA5,
+    "PLAY_VOICE": 0xA6,
+    "EXEC_MACRO": 0xA7,
+    "RESET": 0xA8,
+    "PLAYSPI": 0xA9,
+}
 
 def recv_exact(sock, size):
     buf = bytearray()
@@ -26,18 +30,54 @@ def recv_exact(sock, size):
         buf.extend(chunk)
     return bytes(buf)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.connect((TCP_IP, TCP_PORT))
+def main():
+    parser = argparse.ArgumentParser(
+        description="Send ISD commands to the 360 over TCP"
+    )
+    parser.add_argument(
+        "ip",
+        help="IP and port of the 360 e.g 10.42.0.140:902"
+    )
+    parser.add_argument(
+        "cmd",
+        choices=COMMANDS.keys()
+    )
+    parser.add_argument(
+        "--arg",
+        type=int,
+        help="Argument to send e.g 5 for PLAYSPI",
+        required=False
+    )
+    parser.add_argument(
+        "--norec",
+        help="Don't wait for recived data",
+        required=False
+    )
 
-packet = bytes([ISD1200_PLAYSPI])
-s.send(packet)
+    args = parser.parse_args()
+    
+    TCP_IP, TCP_PORT = args.ip.split(":")
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    s.connect((TCP_IP, int(TCP_PORT)))
+    
+    cmd_hex = COMMANDS[args.cmd]
+    
+    if args.arg is None:
+        packet = bytes([cmd_hex])
+        s.send(packet)
+    else:
+        packet = struct.pack(">BH", cmd_hex, args.arg)
+        s.send(packet)
+        
+    if args.norec is None:
+        data = recv_exact(s, BUFFER_SIZE)
+        s.close()
+        print("Received data:", data.hex())
+    else:
+        s.close()
 
-# packet = struct.pack(">BH", ISD1200_PLAY_VOICE, 5)
-# s.send(packet)
-#
-# data = recv_exact(s, BUFFER_SIZE)
-
-# s.close()
-#
-# print("Received data:", data.hex())
+if __name__ == "__main__":
+    main()
